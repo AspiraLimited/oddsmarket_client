ab70bb5f9a6f46c2dde4d923d2a8a345addd70e5
Ihor Komar <igor.komar@gmail.com>
2022-10-27 17:29:14 +0300

    moved public profile PAT into external maven settings XML

--- feedreader.cmd
+++ feedreader.cmd
@@ -1,5 +1,5 @@
 IF NOT EXIST ".\demo-app-feedreader\target\demo-app-feedreader-1.0-SNAPSHOT-jar-with-dependencies.jar" (
-  mvnw.cmd clean package
+  mvnw.cmd clean package -s demo-app-feedreader/settings-public.xml
 )
 
 java -jar ./demo-app-feedreader/target/demo-app-feedreader-1.0-SNAPSHOT-jar-with-dependencies.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
--- feedreader.sh
+++ feedreader.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -f "./demo-app-feedreader/target/demo-app-feedreader-1.0-SNAPSHOT-jar-with-dependencies.jar" ]; then
-    ./mvnw clean package
+    ./mvnw clean package -s demo-app-feedreader/settings-public.xml
 fi
 
 java -jar ./demo-app-feedreader/target/demo-app-feedreader-1.0-SNAPSHOT-jar-with-dependencies.jar $1 $2 $3 $4 $5 $6 $7 $8 $9
