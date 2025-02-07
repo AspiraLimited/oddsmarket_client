d45d3ccb00f429aaf719005e3ee72d04a7cf6a1d
Ihor Komar <igor.komar@gmail.com>
2025-02-07 09:36:23 +0100

    trading feed DTOs proto3 (test)

--- api/src/test/java/com/aspiralimited/oddsmarket/api/v4/websocket/trading/OddsmarketTradingDtoTest.java
+++ api/src/test/java/com/aspiralimited/oddsmarket/api/v4/websocket/trading/OddsmarketTradingDtoTest.java
@@ -4,8 +4,6 @@ import com.aspiralimited.oddsmarket.api.v4.websocket.trading.dto.OddsmarketTradi
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
