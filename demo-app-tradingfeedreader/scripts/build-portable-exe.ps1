c0ef8b27d8d7ff24d0ecaaf666e0f94ce5f2a80e
andrii7ch <andrii7ch@gmail.com>
2026-04-10 18:15:59 +0300

    Add script for building portable executable trading-feed-reader

---
+++ demo-app-tradingfeedreader/scripts/build-portable-exe.ps1
@@ -0,0 +1,31 @@
+$AppName = "TradingFeedReader"
+$AppVersion = "1.0.0"
+$JarName = "demo-app-tradingfeedreader-1.0-SNAPSHOT-jar-with-dependencies.jar"
+$MainClass = "com.aspiralimited.oddsmarket.client.demo.tradingfeedreader.TradingFeedReader"
+
+Remove-Item .\packaging -Recurse -Force -ErrorAction SilentlyContinue
+
+.\mvnw.cmd -pl demo-app-tradingfeedreader -am clean package -DskipTests -s demo-app-tradingfeedreader\settings-public.xml
+
+New-Item -ItemType Directory -Force -Path .\packaging\input | Out-Null
+New-Item -ItemType Directory -Force -Path .\packaging\dist | Out-Null
+
+Copy-Item ".\demo-app-tradingfeedreader\target\$JarName" ".\packaging\input\$JarName"
+
+jpackage `
+  --type app-image `
+  --name $AppName `
+  --app-version $AppVersion `
+  --dest .\packaging\dist `
+  --input .\packaging\input `
+  --main-jar $JarName `
+  --main-class $MainClass `
+  --arguments "--interactive" `
+  --win-console `
+  --vendor "Aspira" `
+  --description "Portable Trading Feed Reader"
+
+Compress-Archive `
+  -Path ".\packaging\dist\$AppName" `
+  -DestinationPath ".\packaging\$AppName-portable.zip" `
+  -Force
